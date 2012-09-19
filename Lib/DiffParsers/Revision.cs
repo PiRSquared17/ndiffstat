a8b9b3955ddc9a619da4253d6efa752349975761
jeckyhl <jeckyhl@localhost>
2012-09-19 13:22:54 +0000

    Improving class Revision : - handle alphanum changesets - override inherited Equals(object obj) method

--- Lib/DiffParsers/Revision.cs
+++ Lib/DiffParsers/Revision.cs
@@ -17,12 +17,15 @@ namespace NDiffStatLib.DiffParsers
 
 		public Revision( string rev_str )
 		{
-			if (StringUtils.IsNaturalNumberString(rev_str)) {
+			if (StringUtils.IsNaturalNumberString(rev_str)) { 
+				// integer revision number
+				// Subversion revisions for example
 				revNumber = int.Parse(rev_str);
-			} else {
-				if (!rev_str.In(HEAD, UNKNOWN, PRE_CREATION)) {
-					throw new ArgumentNullException("Invalid argument for Revision constructor : '" + rev_str + "'");
-				}
+			} else if (StringUtils.IsAlphaNumString(rev_str)) { 
+				// alpha num value (changeset id)
+				// ok, that's fine
+			} else if (!rev_str.In(HEAD, UNKNOWN, PRE_CREATION)) {
+				throw new ArgumentNullException("Invalid argument for Revision constructor : '" + rev_str + "'");
 			}
 			this.rev_str = rev_str;
 		}
@@ -33,6 +36,19 @@ namespace NDiffStatLib.DiffParsers
 			rev_str = rev_num.ToString();
 		}
 
+		public override bool Equals( object obj )
+		{
+			if (obj == null) return false;
+			Revision rev = obj as Revision;
+			if (rev == null) return false;
+			return this.Equals(rev);
+		}
+
+		public override int GetHashCode()
+		{
+			return rev_str.GetHashCode();
+		}
+		
 		#region IEquatable<Revision> Membres
 		public bool Equals( Revision other )
 		{
