c2fb7d96affb77e75f9d92135931b4bed1a6ae29
jeckyhl <jeckyhl@localhost>
2012-11-17 17:54:59 +0000

    Update comments

--- Lib/DiffStatOptions.cs
+++ Lib/DiffStatOptions.cs
@@ -25,20 +25,19 @@ namespace NDiffStatLib
 		/// </summary>
 		public FormatOption format_opt { get; set; }
 		/// <summary>
+		/// wildcard pattern which allows to include only specifics files/folders in stats
+		/// <example>
+		/// included_files_pattern = { "**/foo/*" }
+		/// excluded_files_pattern = { "**/*.txt" }
+		/// --> takes only files contained in a directory named "foo", whose extension is not .txt
+		/// </example>
+		/// </summary>
+		private readonly List<string> included_files_pattern;
+		/// <summary>
 		/// wildcard pattern which allows to exclude specifics files/folders from stats
 		/// the pattern is matched against the file path relative to the root folder of the diff
 		/// </summary>
 		public readonly List<string> excluded_files_pattern;
-		/// <summary>
-		/// wildcard pattern which allows to include specifics files/folders in stats
-		/// even if they are in the exlusion list (see <c>excluded_files_pattern</c>)
-		/// <example>
-		/// excluded_files_pattern = { "*.cs" }
-		/// included_files_pattern = { "*/foo.cs", "foo.cs" }
-		/// --> all files with "cs" extention will be excluded, except those named "foo.cs"
-		/// </example>
-		/// </summary>
-		public readonly List<string> included_files_pattern;
 
 		public DiffStatOptions()
 		{
